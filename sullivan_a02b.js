742c6776cb07cd58f4e59178a9ca58a2a536181a
amsullivan <58862519+amsullivan@users.noreply.github.com>
2019-12-13 13:37:00 -0600

    Add files via upload

---
+++ sullivan_a02b.js
@@ -0,0 +1,23 @@
+/*  Anna Sullivan
+    sullivan_a02b.js
+    INFO2124WW
+    Thoendel
+    12/13/19
+*/
+{
+let fuName = "Anna Sullivan";
+var age = "25";
+const hairColor = "Blonde";
+const eyeColor = "Brown";
+
+console.log(" ");
+console.log("My name:");
+console.log(fuName);
+console.log("My age:");
+console.log(age);
+console.log("My Hair Color:");
+console.log(hairColor);
+console.log("My Eye Color:");
+console.log(eyeColor);
+console.log(" ");
+}
